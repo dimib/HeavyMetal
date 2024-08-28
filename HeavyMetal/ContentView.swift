acafa2b533dcbd40dcd3cbf6f7d399ea44f23f3c
Dimitri Brukakis <dimitri@brukakis.net>
2024-08-28 20:01:38 +0200

    Initial Commit

---
+++ HeavyMetal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HeavyMetal
+//
+//  Created by Dimitri Brukakis on 28.08.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HeavyMetal/HeavyMetalApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeavyMetalApp.swift
+//  HeavyMetal
+//
+//  Created by Dimitri Brukakis on 28.08.24.
+//
+
+import SwiftUI
+
+@main
+struct HeavyMetalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
